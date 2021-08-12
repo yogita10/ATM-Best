3ef07b6a9bf2f7bc5c49a71a3744409ab9da5225
yogita <yogita10khurana@gmail.com>
2021-08-13 00:23:58 +0530

    update

---
+++ atm.py
@@ -0,0 +1,16 @@
+
+try:
+
+    f = open('Accounts.txt', 'r')
+    f.close()
+except FileNotFoundError:
+    # if 'Accounts.txt' file is not found, create it
+    f = open('Accounts.txt', 'w')
+    f.close()
+
+
+import menu1
+import os
+
+os.system('clear')
+menu1.menu1()  
